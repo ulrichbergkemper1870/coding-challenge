aa53f4ccc158a8ff136d8f8c014715e89dd3618f
Ieva <kaziukonyte.ieva@gmail.com>
2022-11-03 09:44:24 +0000

    Solution wo TS

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <template>
   <h1>Coding Challenge</h1>
-  <Input />
-  <Output />
+  <Input @add-to-list="addToList" :is-add-disabled="isAddDisabled" />
+  <Output :list="list" @remove-from-list="removeFromList" />
 </template>
 
-<script type="ts">
+<script lang="ts">
+import { ref, computed } from "vue";
 import Input from "@/components/Input.vue";
 import Output from "@/components/Output.vue";
 
@@ -14,7 +15,33 @@ export default {
     Output,
   },
   setup() {
-    return {};
+    const list = ref([{ id: Date.now().toString(), value: "Example" }]);
+
+    const addToList = (value) => {
+      const newItem = {
+        id: Date.now().toString(),
+        value,
+      };
+      list.value.push(newItem);
+    };
+
+    const removeFromList = (id) => {
+      const indexOfitemToRemove = list.value.findIndex(
+        (item) => item.id === id
+      );
+      list.value.splice(indexOfitemToRemove, 1);
+    };
+
+    const isAddDisabled = computed(() => {
+      return list.value.length === 10;
+    });
+
+    return {
+      list,
+      addToList,
+      removeFromList,
+      isAddDisabled,
+    };
   },
 };
 </script>
--- src/components/Input.vue
+++ src/components/Input.vue
@@ -1,18 +1,31 @@
 <template>
-  <form>
-    <input type="text" name="textinput" />
-    <button type="submit">Add</button>
+  <form @submit.prevent="onAdd">
+    <input type="text" name="textinput" v-model="inputModel" />
+    <button type="submit" :disabled="isAddDisabled" v-on:keyup.enter="onAdd">
+      Add
+    </button>
   </form>
 </template>
 
-<script type="ts">
+<script lang="ts">
+import { ref } from "vue";
+
 export default {
   props: {
-    // add optional props here
+    isAddDisabled: Boolean,
   },
-  setup() {
-    // setup component
-    return {};
+  setup(_, { emit }) {
+    const inputModel = ref("");
+    const onAdd = () => {
+      if (inputModel.value) {
+        emit("add-to-list", inputModel.value);
+        inputModel.value = "";
+      }
+    };
+    return {
+      inputModel,
+      onAdd,
+    };
   },
 };
 </script>
